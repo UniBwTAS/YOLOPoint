db637f330a96e6175be0bda6857897f1793e7e86
antopost <antopost>
2024-03-07 08:40:17 +0100

    YOLOPoint: commented out line in logger that was causing problems suddenly

--- src/utils/general_yolo.py
+++ src/utils/general_yolo.py
@@ -35,7 +35,7 @@ from utils.metrics_yolo import box_iou, fitness
 # Settings
 torch.set_printoptions(linewidth=320, precision=5, profile='long')
 np.set_printoptions(linewidth=320, formatter={'float_kind': '{:11.5g}'.format})  # format short g, %precision=5
-pd.options.display.max_columns = 10
+# pd.options.display.max_columns = 10
 cv2.setNumThreads(0)  # prevent OpenCV from multithreading (incompatible with PyTorch DataLoader)
 os.environ['NUMEXPR_MAX_THREADS'] = str(min(os.cpu_count(), 8))  # NumExpr max threads
 
